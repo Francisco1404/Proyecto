94bdd5456a5b79f1d07438577d2f58ea3f0001df
KennethRocha7 <126303386+KennethRocha7@users.noreply.github.com>
2023-03-09 19:39:02 -0600

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -17,4 +17,28 @@ while True:
         print("2. Salir") 
         print("/*-----------------------------------------*/") 
         respuesta = int(input()) 
+        if respuesta == 1: 
+            lista = 0
+            while lista != 4:
+                print("Ingrese el número de artículo que quiere comprar") 
+                print("1. Caja de leche (¢ 1200)") 
+                print("2. Pan (¢ 1000)") 
+                print("3. Mantequilla (¢ 510)") 
+                print("4. Terminar lista") 
+                lista = int(input())
+            
+                if listaComedera != "":
+                    agregarSeparadorComa = 1
+            
+                if lista != 4:
+                    cantidad = int(input("Indique la cantidad: ")) # se solicita la cantidad al usuario
+            
+                if lista == 1: 
+                    if agregarSeparadorComa == 1: 
+                        listaComedera = listaComedera + ", " + str(cantidad) + " Caja(s) de Leche (¢ 1200)" 
+                    else: 
+                        listaComedera = listaComedera + str(cantidad) + " Caja(s) de Leche (¢ 1200)"
+                    
+                    costoTotal = costoTotal + (cantidad * 1200)
+                    print("Usted ha seleccionado " + str(cantidad) + " Caja(s) de Leche")
     
