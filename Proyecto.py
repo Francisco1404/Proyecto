92c32e285256872898b981ad00d0aae3d7a7ef77
Francisco1404 <126122080+Francisco1404@users.noreply.github.com>
2023-03-30 18:23:26 -0600

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -1,24 +1,38 @@
-print("Bienvenido al supermercado 'Los Pollitos'")
-nombre=(input('Ingrese su nombre y apellido \n-'))
-fecha=(input('Ingrese la fecha (00/00/0000)\n-'))
-if nombre=='':
-    print("Menú de inicio")
-
-listaComedera = ""
-costoTotal = 0
-
-#Esta variable es para controlar si se debe agregar un separador cada vez que
-#se agrega un artículo a la lista que guardamos en la variable listaComedera.
-agregarSeparadorComa = 0
-
 while True:
-        print("/*-----------------------------------------*/")
-        print("1. Ingresar nueva compra")
-        print("2. Salir")
-        print("/*-----------------------------------------*/")
-        respuesta = int(input())
-        if respuesta == 1:
-            lista = 0
+    print("Bienvenido al supermercado 'Los Pollitos'")
+
+    nombre_valido = False
+    while not nombre_valido:
+        nombre = input('Ingrese su nombre y apellido: ')
+        if len(nombre) > 0:
+            nombre_valido = True
+        else:
+            print('Por favor, ingrese su nombre y apellido.')
+
+    fecha_valida = False
+    while not fecha_valida:
+        fecha = input('Ingrese la fecha (00/00/0000): ')
+        if len(fecha) > 0:
+            fecha_valida = True
+        else:
+            print('Por favor, ingrese la fecha.')
+
+        if nombre=='':
+            print("Menú de inicio")
+    
+    listaComedera = ""
+    costoTotal = 0
+    cantidad = 0
+    
+    reiniciar = True
+    while reiniciar:
+            print("/*-----------------------------------------*/")
+            print("1. Ingresar nueva compra")
+            print("2. Salir")
+            print("/*-----------------------------------------*/")
+            respuesta = int(input())
+            if respuesta == 1: #apartir de este if te da las opciones para comprar
+                lista = 0
             while lista != 4:
                 print("Ingrese el número de artículo que quiere comprar")
                 print("1. Caja de leche (¢ 1200)")
@@ -26,10 +40,16 @@ while True:
                 print("3. Mantequilla (¢ 510)")
                 print("4. Terminar lista")
                 lista = int(input())
-
-                if listaComedera != "":
-                    agregarSeparadorComa = 1
-
+                
+                    if lista > 0 and lista < 16:
+                        valor_valido = False
+                        while not valor_valido:
+                            cantidad = input("Indique la cantidad: ")
+                            if cantidad != "":
+                                cantidad = int(cantidad)
+                                valor_valido = True
+                            else:
+                                print("Por favor, ingrese una cantidad válida.")
                 if lista != 4:
                     cantidad = int(input("Indique la cantidad: ")) # se solicita la cantidad al usuario
 
@@ -61,7 +81,13 @@ while True:
                     print("Esta es su lista de compras: " + listaComedera)
                     print("El total a pagar es: " ,+ costoTotal)
                     print("/*-----------------------------------------*/")
-                else:
-                    print("Opción inválida")
-        else:
-            print("Limpiando lista de compras. Saliendo...")
+            else:
+                print("Limpiando lista de compras. Saliendo...")
+    
+            if respuesta == 2 and cantidad >= 0:
+                #REINICIAR TODO EL SISTEMA
+                reiniciar = False
+            elif lista == 16:
+                   #SE DEBE LIMPIAR LA LISTA DE ARTICULOS
+                   lista = 0
+                   listaComedera = ""
